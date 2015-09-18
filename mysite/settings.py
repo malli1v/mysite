1a1f83d81a45f4af7608de79c5908d30a6806651
malli1v <vinaysandeepm@gmail.com>
2015-09-18 16:24:18 -0400

    fixing STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,4 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-static_root = os.path.join(BASE_DIR, 'STATIC')
+STATIC_ROOT = os.path.join(BASE_DIR, 'STATIC')
